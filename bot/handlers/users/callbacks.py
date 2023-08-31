7cf3e2ac42dac71a35b8cfe9b2031683b65538ba
ovsij <v.ovsij@yandex.ru>
2023-08-31 17:46:25 +0300

    add doors

--- bot/handlers/users/callbacks.py
+++ bot/handlers/users/callbacks.py
@@ -76,62 +76,63 @@ async def btn_callback(callback_query: types.CallbackQuery):
     if re.fullmatch('menu/doors/\d*/\d*', code):
         model_id = int(code.split('/')[-1])
         collection_id = int(code.split('/')[-2])
+        model = get_model(id=model_id)
         collection = get_collection(id=collection_id)
 
-        product_imgs = [p.image for p in get_product(model=model_id) if os.path.exists(p.image) and p.width == '800.0']
+        product_imgs = list(set([p.image for p in get_product(model=model_id) if os.path.exists(p.image) and str(p.width) == '800.0' and p.collection.id == collection_id]))
 
-        path = f"database/Doors/{collection.name}/"
-        print(path)
+        if len(product_imgs) == 0:
+            product_imgs = list(set([p.image for p in get_product(model=model_id) if os.path.exists(p.image) and str(p.width) == '700.0' and p.collection.id == collection_id]))
+        if len(product_imgs) == 0:
+            product_imgs = list(set([p.image for p in get_product(model=model_id) if os.path.exists(p.image) and str(p.width) == '600.0' and p.collection.id == collection_id]))
+        if len(product_imgs) == 0:
+            product_imgs = list(set([p.image for p in get_product(model=model_id) if os.path.exists(p.image) and str(p.width) == '900.0' and p.collection.id == collection_id]))
+        if len(product_imgs) == 0:
+            product_imgs = list(set([p.image for p in get_product(model=model_id) if os.path.exists(p.image) and str(p.width) == '400.0' and p.collection.id == collection_id]))
+
+        path = f"database/Doors/{collection.name}/{model.name}/"
         for root, dirs, files in os.walk(path):
             for filename in sorted(files):
                 if '.jpg' in filename or '.png' in filename:
                     product_imgs.append(f"{path}{filename}")
         
-        for vid in collection.videos.split('//'):
-            #await bot.send_video(callback_query.from_user.id, vid)
-            product_imgs.append(vid)
+        path = f"database/Doors/{collection.name}/"
+        for root, dirs, files_ in os.walk(path):
+            for filename_ in sorted(files_):
+                if '.jpg' in filename_ or '.png' in filename_:
+                    product_imgs.append(f"{path}{filename_}")
+            break
         
+        if collection.videos:
+            for vid in collection.videos.split('//'):
+                product_imgs.append(vid)
         
-        #print(product_imgs)
-        #print(len(product_imgs))
         if len(product_imgs) % 10 > 0:
             r = len(product_imgs) // 10 + 1
         else:
             r = len(product_imgs) / 10
-        print(r)
-        print(len(product_imgs))
+        print(product_imgs)
         for i in range(int(r)):
             photo = []
             for img in product_imgs[i * 10:(i+1) * 10]:
-                
-                if img == product_imgs[-2] or img == product_imgs[-1]:
-                    print(f"vid {product_imgs.index(img)}")
-                    photo.append(types.InputMediaVideo(media=img))
-                    print(f"vid {product_imgs.index(img)}")
-                
-                else:
-                    print(f"img {product_imgs.index(img)}")
+                if img.endswith('.jpg') or img.endswith('.png'):
                     photo.append(types.InputMedia(media=open(img, 'rb')))
-                    print(f"img {product_imgs.index(img)}")
+                else:
+                    photo.append(types.InputMediaVideo(media=img))
+                    
+            try:
+                await bot.send_media_group(
+                    callback_query.message.chat.id, 
+                    media=photo,
+                )
+            except Exception as ex:
+                print(ex)
                 
         
-            #photo = [types.InputMedia(media=open(img, 'rb')) \
-            #        if 'database' in img else types.InputMediaVideo(media=img) for img in product_imgs[i * 10:(i+1) * 10]]
-            print(photo)
-            msg = await bot.send_media_group(
-                callback_query.message.chat.id, 
-                media=photo,
-            )
-            print(msg)
-#BQACAgIAAxkBAAJVImTh-8cKIRlLFaQdYu2REdoB3qSfAALaMgACOhkRS1KDkAbiWr2nMAQ
-# #BQACAgIAAxkBAAJVIWTh-8d11BWVedKd0ynRbYWfx2ooAALPMgACOhkRS1rBo1xodzMUMAQ//BQACAgIAAxkBAAJVIGTh-8f84dbyxQhJ11aMLHUvov4jAALOMgACOhkRS4r5SoEUmS8pMAQ
-# #BQACAgIAAxkBAAJVH2Th-8e5JnDGCdvtJTbRDTUBpf30AALNMgACOhkRSyR4XhxUrPZNMAQ//
+        if collection.documents:
+            for doc in collection.documents.split('//'):
+                await bot.send_document(callback_query.from_user.id, doc)
         
-        for doc in collection.documents.split('//'):
-            await bot.send_document(callback_query.from_user.id, doc)
-            #product_imgs.append(doc)
-        
-
         text, reply_markup = inline_kb_doorsmodel(collection_id=collection_id, model_id=model_id)
         await bot.send_message(
             callback_query.from_user.id,
